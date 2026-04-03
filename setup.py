27b3006e5119f286c1ffb0f59a55f49fd12c94c4
Nguyen Duc Linh <436660@etud.uco.fr>
2026-04-03 02:56:10 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='factor-timing-engine',
-    version='2.0.0',
-    description='Cross-Sector Factor Timing & Dynamic Allocation Engine',
-    packages=find_packages(),
-    python_requires='>=3.11',
-)
